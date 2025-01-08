b2ceac1c6b23eba3e99f0799a139a0c34d737357
Aishwarya Mhetre <mhetreaishwarya07@gmail.com>
2025-01-08 17:13:16 +0530

    React project

---
+++ ReactProjects/brandpage/src/App.js
@@ -0,0 +1,22 @@
+//import logo from './logo.svg';
+//import './App.css';
+
+const App =() =>{
+return <div> 
+  <nav>
+    <div className="logo">
+      <img src="nike_logo.png" alt="logo"/>
+    </div>
+    <ul>
+      <li href="#">Menu</li>
+      <li href="#">Location</li> 
+      <li href="#">About</li>
+      <li href="#">Contact</li>
+    </ul>
+    <button>Login</button>
+  </nav>
+</div>
+  
+
+};
+ export default App;
